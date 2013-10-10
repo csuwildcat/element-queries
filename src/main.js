b9d422aabcf9886da17be2034d25610ddd38e03b
Arron Schaar <aschaar@gmail.com>
2013-10-09 20:56:13 -0700

    Added x-stub

--- src/main.js
+++ src/main.js
@@ -1,5 +1,24 @@
 (function(){  
 
-// Create your component here
+  // Create your component here
+  // http://x-tags.org/docs
+  
+  xtag.register('x-stub', {
+    lifecycle: {
+      created: function() {},
+      inserted: function() {},
+      removed: function() {},
+      attributeChanged: function() {}
+    }, 
+    events: { 
+    
+    },
+    accessors: {
+      
+    }, 
+    methods: {
+      
+    }
+  });
 
 })();
